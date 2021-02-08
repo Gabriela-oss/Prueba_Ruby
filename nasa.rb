19f1ae98baeb434cda51f4018dc3071c85e2865b
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-02-08 14:55:10 -0600

    I added photos count

--- nasa.rb
+++ nasa.rb
@@ -16,22 +16,33 @@ end
 #index HTML
 def buid_web_page(info_hash)
   File.open('appod_index.html','w') do |file|
-  photos = info_hash["photos"]
-  file.puts "<html>"
-  file.puts "<head>"
-  file.puts "</head>"
-  file.puts "<body>"
-  file.puts "<ul>"
-   photos.each do |photo|
-   file.puts "<li><img src='#{photo["img_src"]}'></li>"
-  
-   end 
-   file.puts "</ul>"
-   file.puts "</body>"
-   file.puts "</html>"
+    photos = info_hash["photos"]
+    file.puts "<html>"
+    file.puts "<head>"
+    file.puts "</head>"
+    file.puts "<body>"
+    file.puts "<ul>"
+    photos.each do |photo|
+      file.puts "<li><img src='#{photo["img_src"]}'></li>"
+    end 
+    file.puts "</ul>"
+    file.puts "</body>"
+    file.puts "</html>"
   end
-  end
+end
 
-
+def photos_count(info_hash)
+    photos = {}
+    info_hash['photos'].each  do |photo|
+      camera = photo['camera']['name']
+      if photos[camera]
+        photos[camera] += 1 
+      else 
+        photos[camera] = 1 
+      end 
+    end 
+    photos 
+end 
 nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos","l8k04BFBf0UF3ZKhzOcth68Be656TWdkOn4f8Pv9")
+puts photos_count(nasa_array)
 buid_web_page(nasa_array)
