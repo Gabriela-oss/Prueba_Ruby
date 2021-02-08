9086ad585c9e4091a3e6acdd66b1cd9dcc9569a2
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-02-08 12:34:53 -0600

    I added the img and the html

--- nasa.rb
+++ nasa.rb
@@ -3,7 +3,7 @@ require "net/http"
 require "json"
 
 def request(url,token = nil)
-  url = URI("#{url}?sol=1000&#{token}")
+  url = URI("#{url}?sol=1000&api_key=#{token}")
   https = Net::HTTP.new(url.host, url.port)
   https.use_ssl = true
   request = Net::HTTP::Get.new(url)
@@ -13,5 +13,25 @@ def request(url,token = nil)
 end
 
 
-nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos","api_key=l8k04BFBf0UF3ZKhzOcth68Be656TWdkOn4f8Pv9")
-puts nasa_array
+#index HTML
+def buid_web_page(info_hash)
+  File.open('appod_index.html','w') do |file|
+  photos = info_hash["photos"]
+  file.puts "<html>"
+  file.puts "<head>"
+  file.puts "</head>"
+  file.puts "<body>"
+  file.puts "<ul>"
+   photos.each do |photo|
+   file.puts "<li><img src='#{photo["img_src"]}'></li>"
+  
+   end 
+   file.puts "</ul>"
+   file.puts "</body>"
+   file.puts "</html>"
+  end
+  end
+
+
+nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos","l8k04BFBf0UF3ZKhzOcth68Be656TWdkOn4f8Pv9")
+buid_web_page(nasa_array)
